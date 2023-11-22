cb06b26d13c7445d34316ba13cead9b04382c870
wiederin <wiederinchristoph@gmail.com>
2023-11-22 12:17:58 +0100

    load font

--- build.go
+++ build.go
@@ -8,24 +8,29 @@ import (
 func (doc *Document) Build() (*fpdf.Fpdf, error) {
     // todo: validate document data
 
-	// Build base doc
-	doc.pdf.SetMargins(BaseMargin, BaseMarginTop, BaseMargin)
-	doc.pdf.SetXY(10, 10)
+    // Build base doc
+    doc.pdf.SetMargins(BaseMargin, BaseMarginTop, BaseMargin)
+    doc.pdf.SetXY(10, 10)
 
     // todo: add invoice data
 
-	if doc.Header != nil {
-		if err := doc.Header.applyHeader(doc); err != nil {
-			return nil, err
-		}
-	}
+    if doc.Header != nil {
+        if err := doc.Header.applyHeader(doc); err != nil {
+            return nil, err
+        }
+    }
 
+    if doc.Footer != nil {
+        if err := doc.Footer.applyFooter(doc); err != nil {
+            return nil, err
+        }
+    }
 
-	if doc.Footer != nil {
-		if err := doc.Footer.applyFooter(doc); err != nil {
-			return nil, err
-		}
-	}
+    // Add first page
+    //doc.pdf.AddPage()
 
-	return doc.pdf, nil
+    // Load font
+    doc.pdf.SetFont(doc.Config.Font, "", 12)
+
+    return doc.pdf, nil
 }
